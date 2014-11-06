03b52f78dcc56e31a9c00321da036a8cc7cc7362
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-06 08:58:24 -0600

    d851: de-commonize thermal-engine config
    Change-Id: I52f7c82be5a8eba44e26bdc46c8ef532890b21ce

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ $(call inherit-product-if-exists, vendor/lge/d851/d851-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
+
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface
