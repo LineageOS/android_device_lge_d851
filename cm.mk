19ce129210cf29d847adba66c157d0ec29f81614
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-08 15:23:50 -0600

    overlay: prefer the ipv6 apn
    * also add overlay after vendor/cm so we're sure we aren't getting
      overlayed by that overlay.
    
    Change-Id: I53a0de73b69b281fce7ee9eb4cc17fe740a5efc4

--- cm.mk
+++ cm.mk
@@ -6,6 +6,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 PRODUCT_NAME := cm_d851
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
