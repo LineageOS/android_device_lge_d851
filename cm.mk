ff5a7ba5fada0780d071d44347dbb48b9fd06e7f
bryan2894 <bryan2894@gmail.com>
2015-11-22 01:43:25 -0500

    d851: update fingerprint from D85120G
    Change-Id: Ib9bfb8dab4b79bba61801afe72defa5d7792065d

--- cm.mk
+++ cm.mk
@@ -10,4 +10,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := cm_d851
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_tmo_us" \
+    BUILD_FINGERPRINT="lge/g3_tmo_us/g3:5.0.1/LRX21Y/15227151285fc:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_tmo_us-user 5.0.1 LRX21Y 15227151285fc release-keys"
