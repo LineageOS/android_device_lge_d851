028e8dc09d7b6f226f10f5551fd7e2daf8c53d36
bryan2894 <bryan2894@gmail.com>
2016-11-22 12:46:38 -0500

    d851: Remove enhanced NFC feature
    Change-Id: Ic7ec972ac1c502df1c31f77347148fe0684aaaf2

--- cm.mk
+++ cm.mk
@@ -5,9 +5,6 @@ $(call inherit-product, device/lge/d851/full_d851.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 PRODUCT_NAME := cm_d851
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
