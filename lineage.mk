066db27155b27665923bfce0a348e4c7dec51d79
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-02 13:09:42 -0600

    d851: cm->lineage
    Change-Id: Icc01c3e3bb1178d0492eeafcbb782a823045a3e2

--- lineage.mk
+++ lineage.mk
@@ -2,10 +2,10 @@ PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 $(call inherit-product, device/lge/d851/full_d851.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_d851
+PRODUCT_NAME := lineage_d851
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
