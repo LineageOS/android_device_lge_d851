7a3c093e9b73d3b3b55096da117a060955d297c9
Aleksander Gencel <johnnylittleplanet@gmail.com>
2018-12-15 14:31:09 +0100

    d851: add PRODUCT_GMS_CLIENTID_BASE
    Change-Id: I03d671cc800943b6466b8261a1184f11dc626a79

--- lineage.mk
+++ lineage.mk
@@ -13,3 +13,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="g3_tmo_us-user 6.0 MRA58K 1609517159bca release-keys"
 
 BUILD_FINGERPRINT="lge/g3_tmo_us/g3:6.0/MRA58K/1609517159bca:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-lge
