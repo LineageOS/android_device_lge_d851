d181bc51247a559acdaaab96cb336483aaccb8f3
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-19 23:26:07 +0100

    d851: Migrate from system.prop to system_prop.mk
     * This is the new way to do things as of AOSP 8.0.
     * This will allow us to split system/vendor properties if we want to.
    
    Change-Id: Ia147eb39342122bf0ecdcd3f2d3c2338569fc2da

---
+++ system_prop.mk
@@ -0,0 +1,9 @@
+ #
+# System properties for d851
+#
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
+    telephony.lteOnGsmDevice=1 \
+    ro.telephony.default_network=9
